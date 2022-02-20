e3e25630a620f5c2873489ca7da3e8e79e5115c8
hari-ls <hari@logicsync.com.au>
2022-02-21 01:05:30 +1030

    added code to write generated file

---
+++ src/file.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+// create HTML file with the data
+const createFile = (data) => {
+  const root = path.resolve(__dirname, "../");
+  const dist = path.resolve(__dirname, "../dist");
+  // check if the directory exists, if note create one
+  if (!fs.existsSync(dist)) {
+    console.log("The path doesn't exist.");
+    fs.mkdir(`${root}/dist`, (err) => {
+      if (err) throw err;
+    });
+  }
+  // create file with the data supplied
+  fs.writeFile(`${dist}/index.html`, data, (err) => {
+    if (err) throw err;
+  });
+};
+
+// createFile("<p>Hello World!</p>");
+module.exports = createFile;
